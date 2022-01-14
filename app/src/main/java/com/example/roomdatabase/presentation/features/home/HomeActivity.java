3a151b73f16829c971daf26819e9e9da90c3fe5f
JoseNguyen <abc@example.com>
2022-01-14 18:55:04 +0700

    homeActivity

--- app/src/main/java/com/example/roomdatabase/presentation/features/home/HomeActivity.java
+++ app/src/main/java/com/example/roomdatabase/presentation/features/home/HomeActivity.java
@@ -1,14 +1,16 @@
-package com.example.roomdatabase;
+package com.example.roomdatabase.presentation.features.home;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.roomdatabase.R;
+
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
     }
 }
